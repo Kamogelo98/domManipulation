74ded9c222ab14cb66232d5912f197746483239e
KellyG12 <81985625+KellyG12@users.noreply.github.com>
2021-08-04 23:10:38 +0200

    About page

---
+++ about.js
@@ -0,0 +1,20 @@
+
+
+/*ABOUT PAGE*/
+const mainDiv = document.querySelector('.content');
+const contentsBox = document.createElement('article');
+const h1 = document.createElement('h1');
+h1.textContent = "ABOUT US";
+
+const para1 = document.createElement('section');
+para1.setAttribute('class', 'paragraph1');
+para1.textContent = "At Readers Paradise our lovely visitors will have access to The World's Largest Ebook Library, The Z-Library! Every month the most popular books are highlighted for your convenience. Please feel free to send us a query or suggestion via our contact form!";
+
+const para2 = document.createElement('section');
+para2.setAttribute('class', 'paragraph1');
+para2.textContent = "'I have a passion for teaching kids to become readers, to become comfortable with a book, not daunted. Books shouldn't be daunting, they should be funny, exciting and wonderful; and learning to be a reader gives a terrific advantage. -Roald Dahl'"
+
+contentsBox.appendChild(para1);
+contentsBox.appendChild(para2);
+mainDiv.appendChild(h1);
+mainDiv.appendChild(contentsBox);
--- contact.js
+++ contact.js
@@ -1,25 +1,15 @@
 
+
+/*CONTACT PAGE*/
 /*Submit Button Function*/
 function submission() {
     alert("THANK YOU FOR YOUR SUBMISSION TO READERS PARADISE!");
     return true;
  }
 
-
-
-/*Homepage button*/
-let  buttonPrev = document.createElement('button');
-buttonPrev.classList.add('buttonPrev');
-const main1 = document.querySelector('.main1');
-const prev = document.createElement('a');
-prev.setAttribute("class", "prev")
-prev.textContent = '<<<<< BACK TO HOME';
-prev.href = 'index.html'
-
-buttonPrev.appendChild(prev);
-main1.appendChild(buttonPrev);
-
-
-
-
+/*HeaderDiv*/
+const header = document.getElementById('header1');
+const headerDiv = document.createElement('div');
+headerDiv.classList.add('headerDiv');
+header.appendChild(headerDiv);
 
