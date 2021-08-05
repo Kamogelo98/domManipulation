8da9604e25b41b023d5cddca8663c4ecb3c5685f
KellyG12 <81985625+KellyG12@users.noreply.github.com>
2021-08-05 09:28:53 +0200

    Edited about page

--- about.js
+++ about.js
@@ -20,7 +20,7 @@ para1.textContent = "At Readers Paradise our lovely visitors will have access to
 
 const para2 = document.createElement('section');
 para2.setAttribute('class', 'paragraph1');
-para2.textContent = "'I have a passion for teaching kids to become readers, to become comfortable with a book, not daunted. Books shouldn't be daunting, they should be funny, exciting and wonderful; and learning to be a reader gives a terrific advantage. -Roald Dahl'"
+para2.textContent = "'I have a passion for teaching kids to become readers, to become comfortable with a book, not daunted. Books shouldn't be daunting, they should be funny, exciting and wonderful; and learning to be a reader gives a terrific advantage.' -Roald Dahl"
 
 contentsBox.appendChild(para1);
 contentsBox.appendChild(para2);
